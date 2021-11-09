b62c0adfff80aae382baf5ee3ca0e5416ba9998e
Sarah Etter <6814198+etters@users.noreply.github.com>
2021-11-09 23:05:47 +0100

    needed to get rid of "keys" warning

--- app/screens/components/QuestionsNumbers.js
+++ app/screens/components/QuestionsNumbers.js
@@ -1,12 +1,12 @@
 import React from "react";
-import {View, TouchableOpacity, StyleSheet} from "react-native";
+import {View, Text, TouchableOpacity, StyleSheet} from "react-native";
 import examQuestions from "../examQuestions";
 
 export default function QuestionsNumber(props) {
     return (
     <View style={{flexDirection: "row", padding: 50,}}>
         {examQuestions.results.map((value, index) => (
-            <TouchableOpacity style={{backgroundColor: props.qNumber===index ? "#2196F3": "#F0FFFF", 
-            padding:20, borderRadius:"50%", margin:"#2196F3"}}>{index+1}</TouchableOpacity>))}
+            <TouchableOpacity key={index} style={{backgroundColor: props.qNumber===index ? "#2196F3": "#F0FFFF", 
+            padding:20, borderRadius:"50%", margin:"#2196F3"}}><Text>{index+1}</Text></TouchableOpacity>))}
     </View>);
 };
