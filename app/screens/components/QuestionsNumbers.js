c045f9879068139da077b06311c45851363c2852
bkty <83908855+bkty@users.noreply.github.com>
2021-11-10 16:57:06 +0100

    fixed error for questions numbers, no styling applied

--- app/screens/components/QuestionsNumbers.js
+++ app/screens/components/QuestionsNumbers.js
@@ -1,12 +1,12 @@
 import React from "react";
-import {View, Text, TouchableOpacity, StyleSheet} from "react-native";
+import {View, Button, StyleSheet} from "react-native";
 import examQuestions from "../examQuestions";
 
 export default function QuestionsNumber(props) {
     return (
     <View style={{flexDirection: "row", padding: 50,}}>
         {examQuestions.results.map((value, index) => (
-            <TouchableOpacity key={index} style={{padding:20, borderRadius:40}}><Text>{index+1}</Text></TouchableOpacity>))}
+            <View key={index} style={{backgroundColor: props.qNumber===index ? "#2196F3": "#F0FFFF", padding:20, borderRadius:60,}}><Button title={`${index+1}`}/></View>))}
     </View>);
 };
 
