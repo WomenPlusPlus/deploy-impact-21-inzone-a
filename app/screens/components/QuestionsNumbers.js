7c4b461b42f081941c390fc86127936fb7b7bee9
bkty <83908855+bkty@users.noreply.github.com>
2021-11-11 10:28:29 +0100

    fix style for questions numbers

--- app/screens/components/QuestionsNumbers.js
+++ app/screens/components/QuestionsNumbers.js
@@ -1,12 +1,12 @@
 import React from "react";
-import {View, Button, StyleSheet} from "react-native";
+import {View, Text, TouchableOpacity,} from "react-native";
 import examQuestions from "../examQuestions";
 
 export default function QuestionsNumber(props) {
     return (
     <View style={{flexDirection: "row", padding: 50,}}>
         {examQuestions.results.map((value, index) => (
-            <View key={index} style={{backgroundColor: props.qNumber===index ? "#2196F3": "#F0FFFF", padding:20, borderRadius:60,}}><Button title={`${index+1}`}/></View>))}
+            <View key={index} style={{backgroundColor: props.qNumber===index ? "#2196F3": "#F0FFFF", padding:20, borderRadius:60,}}><TouchableOpacity><Text>{index+1}</Text></TouchableOpacity></View>))}
     </View>);
 };
 
