da3007c754232a2e18e16a62ea8e47963180b0fb
bkty <83908855+bkty@users.noreply.github.com>
2021-11-12 12:24:20 +0100

    add minimal style to Submit screen

--- app/screens/components/ExamSubmitScreen.js
+++ app/screens/components/ExamSubmitScreen.js
@@ -1,9 +1,44 @@
 import React from "react";
-import {View, Text} from "react-native"
+import {View, Text, TouchableOpacity, StyleSheet} from "react-native";
+import Header from "./Header";
+import Timer from "./Timer";
 
 
 export default function ExamSubmitScreen() {
 return (
-
-    <View><Text>THE END</Text></View>);
+    <View style={styles.container}>
+     <Header/>  
+     <Timer/>
+    <View style={styles.textContainer}>
+    <Text>1 of 6 to be checked again</Text> 
+    <Text>1 of 6 not answered</Text>   
+    </View>
+    <View style={styles.buttonsContainer}>
+    <TouchableOpacity style={styles.button}><Text>Start Review</Text></TouchableOpacity>
+    <TouchableOpacity style={styles.button}><Text>Submit Exam</Text></TouchableOpacity>   
+    </View> 
+    </View>);
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: "center",
+        paddingHorizontal: 10
+      },
+      textContainer : {
+          marginVertical: 30,
+
+      },
+      button: {
+        alignItems: "center",
+        backgroundColor: "#778899",
+        padding: 10
+      },
+      buttonsContainer :{
+          padding: 20,
+      }
+
+}
+
+)
