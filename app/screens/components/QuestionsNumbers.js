078c2a84169061b6157b2ed75546e6f44a9de3df
Sarah Etter <6814198+etters@users.noreply.github.com>
2021-11-17 17:10:13 +0100

    Update QuestionsNumbers.js
    get data from props

--- app/screens/components/QuestionsNumbers.js
+++ app/screens/components/QuestionsNumbers.js
@@ -1,11 +1,12 @@
 import React from "react";
 import {View, Text, TouchableOpacity,} from "react-native";
-import examQuestions from "../examQuestions";
 
 export default function QuestionsNumber(props) {
+    let examQuestionsJ = JSON.parse(props.qDATA);//const versus let????
+
     return (
     <View style={{flexDirection: "row", padding: 50,}}>
-        {examQuestions.results.map((value, index) => (
+        {examQuestionsJ.map((value, index) => (
             <View key={index} style={{backgroundColor: props.qNumber===index ? "#2196F3": "#F0FFFF", padding:20, borderRadius:60,}}><TouchableOpacity onPress={()=>props.setQuestion(index)}><Text>{index+1}</Text></TouchableOpacity></View>))}
     </View>);
 };
