54f29c02bca5381e3a014d4915ada121e0d4cb54
bkty <83908855+bkty@users.noreply.github.com>
2021-11-23 12:17:27 +0100

    add timer alerts

--- app/screens/components/Timer.js
+++ app/screens/components/Timer.js
@@ -1,37 +1,71 @@
-import React, {useEffect, useState} from "react";
-import {View, Text, TouchableOpacity} from "react-native"
+import React, { useEffect, useState } from "react";
+import { View, Text, TouchableOpacity } from "react-native";
 
 export default function Timer(props) {
-    const [isTimerVisible, setIsTimerVisible] = useState(false)
-    const [timerCount, setTimerCount] = useState(7200)
-    
-    useEffect(() => {
-        let interval = setInterval(()=> {
-            setTimerCount (lastTimerCount=> {lastTimerCount<=1 && clearInterval (interval)
-            
-                return lastTimerCount -1
-        })
-    },1000)
-    return () => {clearInterval(interval)}},[]);
-    
-    function secondsToHms(time) {
-        
-        var h = Math.floor(time / 3600);
-        var m = Math.floor(time % 3600 / 60);
-        var s = Math.floor(time % 3600 % 60);
-    
-        var hDisplay = h > 0 ? h + ":" : "";
-        var mDisplay = m > 0 ? m + ":": "";
-        var sDisplay = s > 0 ? s : "";
-        return hDisplay + mDisplay + sDisplay; 
-    }
-
-    function handleToggle() {
-        setIsTimerVisible(!isTimerVisible)}
-    
-    
+  const examDuration = 7200; //total duration of exam  in seconds
+  const [isTimerVisible, setIsTimerVisible] = useState(false);
+  const [timerCount, setTimerCount] = useState(examDuration);
 
 
-return <View><TouchableOpacity onPress={handleToggle}><Text style={{fontWeight:"bold"}}>{isTimerVisible ? secondsToHms(timerCount) : "🕒 Timer"}</Text></TouchableOpacity></View>;
+  useEffect(() => {
+    let interval = setInterval(() => {
+      setTimerCount((lastTimerCount) => {
+        lastTimerCount <= 0 && clearInterval(interval);
+        if (lastTimerCount === 3600 || lastTimerCount===1800 || lastTimerCount===600) {
+          alert("Remaining time: " + displayTimeAlerts(secondsToHms(lastTimerCount)[0], secondsToHms(lastTimerCount)[1], secondsToHms(lastTimerCount)[2]));
+        }
+        if (lastTimerCount === 0) {
+          alert("Your time is up. The exam will be submitted.");
+          setIsTimerVisible(false);
+        }
+        return lastTimerCount - 1;
+      });
+    }, 1000);
+    return () => {
+      clearInterval(interval);
+    };
+  }, []);
 
-};
+  function secondsToHms(time) {
+    var h = Math.floor(time / 3600);
+    var m = Math.floor((time % 3600) / 60);
+    var s = Math.floor((time % 3600) % 60);
+    return [h, m, s];
+  }
+
+  function displayTimeAlerts(h,m,s){
+      if (h>0) {
+          var alert = h + (h===1 ? " hour" : " hours")
+      }
+      else {alert = m + " minutes"}
+     
+      return alert;
+  }
+
+  function displayTimer(h, m, s) {
+    var hDisplay = h > 0 ? h + ":" : "";
+    var mDisplay = m > 9 ? m + ":" : "0" + m + ":";
+    var sDisplay = s > 9 ? s : "0" + s;
+    return hDisplay + mDisplay + sDisplay;
+  }
+
+  function handleToggle() {
+    setIsTimerVisible(!isTimerVisible);
+  }
+
+  return (
+    <View>
+      <TouchableOpacity onPress={handleToggle}>
+        <Text style={{ fontWeight: "bold" }}>
+          {isTimerVisible
+            ? displayTimer(
+                secondsToHms(timerCount)[0],
+                secondsToHms(timerCount)[1],
+                secondsToHms(timerCount)[2]
+              )
+            : "🕒 Timer"}
+        </Text>
+      </TouchableOpacity>
+    </View>
+  );
+}
